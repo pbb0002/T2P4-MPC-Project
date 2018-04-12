5a2d7966e4c8abf3687a6ef45d7d2fe38ba5568d
pbb0002 <blandford4@gmail.com>
2018-04-11 23:40:02 -0500

    First commit

---
+++ install-ubuntu.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+apt-get update
+apt-get -y install git libuv1-dev libssl-dev gcc g++ cmake make
+
+git clone https://github.com/uWebSockets/uWebSockets
+cd uWebSockets
+git checkout e94b6e1
+mkdir build
+cd build
+cmake ..
+make
+make install
+
+cd ../..
+ln -s /usr/lib64/libuWS.so /usr/lib/libuWS.so
+rm -r uWebSockets
